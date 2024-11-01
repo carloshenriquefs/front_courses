b6e2b09121e810eddb265d57f415a8d291e3fdb8
Carlos Henrique <ichfs9108@gmail.com>
2024-11-01 14:12:41 -0300

    created project

---
+++ front_courses/src/main/java/br/com/courses/front/courses/FrontCoursesApplication.java
@@ -0,0 +1,13 @@
+package br.com.courses.front.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontCoursesApplication.class, args);
+	}
+
+}
---
+++ front_courses/src/test/java/br/com/courses/front/courses/FrontCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.courses.front.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
